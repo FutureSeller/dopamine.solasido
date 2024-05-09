02c2ead95a12d537abea6c6b43fe4c1335c33cf2
FutureSeller <f.s3ll3r@gmail.com>
2024-05-10 02:11:59 +0900

    fix: title text size

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -25,7 +25,7 @@ export const Profile = ({
         )}
       </Link>
       <div>
-        <h1 className="text-base font-semibold text-2xl">
+        <h1 className="font-semibold text-xl">
           <Link
             href={profile?.link!}
             target="_blank"
