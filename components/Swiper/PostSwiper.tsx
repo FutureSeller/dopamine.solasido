331b9b779d32bba0efae17afc3cc6ea5869c1ffc
FutureSeller <f.s3ll3r@gmail.com>
2024-05-10 02:07:24 +0900

    add goto icon

--- components/Swiper/PostSwiper.tsx
+++ components/Swiper/PostSwiper.tsx
@@ -35,14 +35,26 @@ export const PostSwiper = ({
           />
           <Link
             className={[
-              "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2",
+              "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-flex items-center",
               "bg-black text-white hover:bg-amber-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 z-20",
             ].join(" ")}
             href={`${post?.link}`}
             target="_blank"
             rel="noreferrer"
           >
-            더 보러가기
+            더 보러가기&nbsp;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z"
+                clip-rule="evenodd"
+              />
+            </svg>
           </Link>
         </div>
       </SwiperSlide>
