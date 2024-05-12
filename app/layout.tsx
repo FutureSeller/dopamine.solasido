61c750eff8e0deb3503ad7eef05b6359aa3490b0
FutureSeller <f.s3ll3r@gmail.com>
2024-05-12 13:58:35 +0900

    feat: add meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -38,6 +38,9 @@ export const metadata: Metadata = {
         alt: "그냥, 여우!",
       },
     ],
+  },
+  other: {
+    viewport: "width=device-width, initial-scale=1.0, viewport-fit=cover",
   },
 };
 
