6202e1f68cf155fde8caed920f1b4c6d18f5ee41
FutureSeller <f.s3ll3r@gmail.com>
2024-05-16 03:41:06 +0900

    fix: add active state to tag

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -4,7 +4,7 @@ export const Tag = (props: { tag: { name: string; slug: string | null } }) => {
   return (
     <Link
       href={`/tag/${props.tag?.slug}`}
-      className="flex px-6 py-2 rounded-full whitespace-nowrap border border-white"
+      className="flex px-6 py-2 rounded-full whitespace-nowrap border border-white active:bg-gray-700 transition-all duration-300 ease-in-out"
       scroll={false}
       passHref
     >
