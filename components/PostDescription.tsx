f9c43228e3e8d60f271b53d458f345a2ce32297e
FutureSeller <f.s3ll3r@gmail.com>
2024-05-16 03:56:10 +0900

    chore: add error message

--- components/PostDescription.tsx
+++ components/PostDescription.tsx
@@ -5,10 +5,20 @@ export const PostDescription = ({
 }: {
   post: Database["public"]["Tables"]["POST"]["Row"] | null;
 }) => {
+  if (post === null) {
+    return (
+      <div className="p-4 text-white">
+        <h2 className="text-xl font-semibold">
+          앗! 알수없는 오류가 발생했어요.
+        </h2>
+      </div>
+    );
+  }
+
   return (
     <div className="p-4 text-white">
-      <h2 className="text-xl font-semibold">{post?.title}</h2>
-      <p className="pt-2 break-keep">{post?.description}</p>
+      <h2 className="text-xl font-semibold">{post.title}</h2>
+      <p className="pt-2 break-keep">{post.description}</p>
     </div>
   );
 };
