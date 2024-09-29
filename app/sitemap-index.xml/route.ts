799da4b59e8c8287e0cadf2505c740b177541005
FutureSeller <f.s3ll3r@gmail.com>
2024-09-30 01:14:32 +0900

    fix: sitemap-index content-type

--- app/sitemap-index.xml/route.ts
+++ app/sitemap-index.xml/route.ts
@@ -1,38 +1,38 @@
-import { createClient } from '../../utils/supabase/server';
+import { createClient } from "../../utils/supabase/server";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 const MAX_URL_COUNT = 50000;
 
 const generateSitemapLink = (url: string) =>
-	`<sitemap><loc>${url}</loc></sitemap>`;
+  `<sitemap><loc>${url}</loc></sitemap>`;
 
 export async function GET() {
-	const supabase = createClient();
-	const { count } = await supabase.from('POST').select('id');
-	const postXmls = count
-		? Array.from({ length: Math.ceil(count / MAX_URL_COUNT) }, (_, i) => ({
-				id: i,
-			}))
-		: [{ id: 0 }];
+  const supabase = createClient();
+  const { count } = await supabase.from("POST").select("id");
+  const postXmls = count
+    ? Array.from({ length: Math.ceil(count / MAX_URL_COUNT) }, (_, i) => ({
+        id: i,
+      }))
+    : [{ id: 0 }];
 
-	const sitemapIndexXML = `<?xml version="1.0" encoding="UTF-8"?>
+  const sitemapIndexXML = `<?xml version="1.0" encoding="UTF-8"?>
     <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${generateSitemapLink('https://dopamine.solasido.world/sitemap.xml')}
+      ${generateSitemapLink("https://dopamine.solasido.world/sitemap.xml")}
       ${postXmls
-				.map((xmlId) =>
-					generateSitemapLink(
-						`https://dopamine.solasido.world/post/sitemap/${xmlId.id}.xml`,
-					),
-				)
-				.join('')}
+        .map((xmlId) =>
+          generateSitemapLink(
+            `https://dopamine.solasido.world/post/sitemap/${xmlId.id}.xml`
+          )
+        )
+        .join("")}
       ${generateSitemapLink(
-				`https://dopamine.solasido.world/tag/sitemap/0.xml`,
-			)}
+        `https://dopamine.solasido.world/tag/sitemap/0.xml`
+      )}
     </sitemapindex>
   `;
 
-	return new Response(sitemapIndexXML, {
-		headers: { 'Content-Type': 'text/xml' },
-	});
+  return new Response(sitemapIndexXML, {
+    headers: { "Content-Type": "application/xml" },
+  });
 }
